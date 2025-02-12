e6b32e7ca78f335327ca4075464ffbd97579400c
Manoj Menon <47050165+manojmenon1@users.noreply.github.com>
2025-02-12 00:23:37 -0500

    Uploading updated files to the Github for Machine Failure Prediction project

--- data-science/src/prep.py
+++ data-science/src/prep.py
@@ -8,6 +8,7 @@ import argparse
 from pathlib import Path
 import os
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import mlflow
 
@@ -27,7 +28,13 @@ def main(args):
     '''Read, split, and save datasets'''
 
     # Reading Data
-    df = pd.read_csv(args.raw_data)    
+    df = pd.read_csv(args.raw_data)
+    
+    # Encoding the categorical 'Type' column
+    # Note: We should ideally use one-hot encoding here as there's no inherent order between the categories
+    # However, as we're using a decision tree model, label encoding also works here
+    label_encoder = LabelEncoder()
+    df['Type'] = label_encoder.fit_transform(df['Type'])
 
     # Split Data into train and test datasets
     train_df, test_df = train_test_split(df, test_size=args.test_train_ratio, random_state=42)
